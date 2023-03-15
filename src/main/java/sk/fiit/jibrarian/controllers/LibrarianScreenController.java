98d8f4fe6f3436e84be88af6c449668eaee7cd25
Miroslav Todorovic <mtodorovic81@gmail.com>
2023-03-15 16:41:57 +0100

    Added style to NavBar with style.css

--- src/main/java/sk/fiit/jibrarian/controllers/LibrarianScreenController.java
+++ src/main/java/sk/fiit/jibrarian/controllers/LibrarianScreenController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import java.io.IOException;
@@ -19,23 +21,42 @@ public class LibrarianScreenController implements Initializable {
     private BorderPane bp;
     @FXML
     private AnchorPane ap;
+    @FXML
+    private ToggleButton lib_btn, add_book_btn, borrow_btn;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        loadScreenPart("../views/librarian_library_screen.fxml");
+        lib_btn.setSelected(true);
+        lib_btn.setDisable(true);
     }
 
     @FXML
     public void library(ActionEvent actionEvent) {
         loadScreenPart("../views/librarian_library_screen.fxml");
+        lib_btn.setDisable(true);
+        add_book_btn.setDisable(false);
+        borrow_btn.setDisable(false);
+        add_book_btn.setSelected(false);
+        borrow_btn.setSelected(false);
     }
 
     @FXML
     public void add_book(ActionEvent actionEvent) {
         loadScreenPart("../views/librarian_add_book_screen.fxml");
+        add_book_btn.setDisable(true);
+        lib_btn.setDisable(false);
+        borrow_btn.setDisable(false);
+        lib_btn.setSelected(false);
+        borrow_btn.setSelected(false);
     }
     @FXML
     public void borrowed_books(ActionEvent actionEvent) {
         loadScreenPart("../views/librarian_borrowed_books_screen.fxml");
+        borrow_btn.setDisable(true);
+        lib_btn.setDisable(false);
+        add_book_btn.setDisable(false);
+        lib_btn.setSelected(false);
+        add_book_btn.setSelected(false);
     }
 
     private void loadScreenPart(String part){
