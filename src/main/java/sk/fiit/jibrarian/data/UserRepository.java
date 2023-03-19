836b7d86bc28c7fea0bcfdd767c0b8818ffdd4d3
Nesquiko <lukas.castven@gmail.com>
2023-03-19 15:11:23 +0100

    Update, get librarians and get admins functionality

--- src/main/java/sk/fiit/jibrarian/data/UserRepository.java
+++ src/main/java/sk/fiit/jibrarian/data/UserRepository.java
@@ -2,6 +2,7 @@ package sk.fiit.jibrarian.data;
 
 import sk.fiit.jibrarian.model.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository {
@@ -12,12 +13,34 @@ public interface UserRepository {
     void saveUser(User user) throws AlreadyExistingUserException;
 
     /**
-     * Return user with matching email, it doesn't exist, returns empty optional
+     * Return user with matching email, if it doesn't exist, returns empty optional
      */
     Optional<User> getUserByEmail(String email);
 
+    /**
+     * Updates user, if it doesn't exist, throws UserNotFound
+     */
+    void updateUser(User user) throws UserNotFound;
+
+    /**
+     * Returns all users which are librarians
+     */
+    List<User> getAllLibrarians();
+
+    /**
+     * Returns all users which are admins
+     */
+    List<User> getAllAdmins();
+
+
     class AlreadyExistingUserException extends Exception {
         public AlreadyExistingUserException(String message) {
+            super(message);
+        }
+    }
+
+    class UserNotFound extends Exception {
+        public UserNotFound(String message) {
             super(message);
         }
     }
--- src/main/java/sk/fiit/jibrarian/data/impl/InMemoryUserRepository.java
+++ src/main/java/sk/fiit/jibrarian/data/impl/InMemoryUserRepository.java
@@ -1,8 +1,10 @@
 package sk.fiit.jibrarian.data.impl;
 
 import sk.fiit.jibrarian.data.UserRepository;
+import sk.fiit.jibrarian.model.Role;
 import sk.fiit.jibrarian.model.User;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -22,5 +24,24 @@ public class InMemoryUserRepository implements UserRepository {
     @Override
     public Optional<User> getUserByEmail(String email) {
         return Optional.ofNullable(users.get(email));
+    }
+
+    @Override
+    public void updateUser(User user) throws UserNotFound {
+        if (!users.containsKey(user.getEmail())) {
+            // TODO Log it
+            throw new UserNotFound(String.format("User with id %s doesn't exist", user.getId()));
+        }
+        users.put(user.getEmail(), user);
+    }
+
+    @Override
+    public List<User> getAllLibrarians() {
+        return users.values().stream().filter(user -> user.getRole() == Role.LIBRARIAN).toList();
+    }
+
+    @Override
+    public List<User> getAllAdmins() {
+        return users.values().stream().filter(user -> user.getRole() == Role.ADMIN).toList();
     }
 }
