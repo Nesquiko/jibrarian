8201b127e5f1b5980136ea0c945630dd2465e4d7
Nesquiko <lukas.castven@gmail.com>
2023-03-19 15:15:58 +0100

    Add tests to InMemoryUserRepo

--- src/test/java/sk/fiit/jibrarian/data/impl/InMemoryUserRepositoryTest.java
+++ src/test/java/sk/fiit/jibrarian/data/impl/InMemoryUserRepositoryTest.java
@@ -3,6 +3,7 @@ package sk.fiit.jibrarian.data.impl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import sk.fiit.jibrarian.data.UserRepository.AlreadyExistingUserException;
+import sk.fiit.jibrarian.data.UserRepository.UserNotFound;
 import sk.fiit.jibrarian.model.Role;
 import sk.fiit.jibrarian.model.User;
 
@@ -38,4 +39,36 @@ class InMemoryUserRepositoryTest {
         assertThrows(AlreadyExistingUserException.class, () -> inMemoryUserRepository.saveUser(userWithSameEmail));
     }
 
+    @Test
+    void updateUserNotFound() {
+        assertThrows(UserNotFound.class, () -> inMemoryUserRepository.updateUser(user));
+    }
+
+    @Test
+    void updateUserSuccessfully() throws AlreadyExistingUserException, UserNotFound {
+        inMemoryUserRepository.saveUser(user);
+        user.setRole(Role.ADMIN);
+        inMemoryUserRepository.updateUser(user);
+        var userFromRepo = inMemoryUserRepository.getUserByEmail(user.getEmail());
+        assertTrue(userFromRepo.isPresent());
+        assertEquals(user, userFromRepo.get());
+    }
+
+    @Test
+    void getAllLibrarians() throws AlreadyExistingUserException {
+        user.setRole(Role.LIBRARIAN);
+        inMemoryUserRepository.saveUser(user);
+        var librarians = inMemoryUserRepository.getAllLibrarians();
+        assertEquals(1, librarians.size());
+        assertEquals(user, librarians.get(0));
+    }
+
+    @Test
+    void getAllAdmins() throws AlreadyExistingUserException {
+        user.setRole(Role.ADMIN);
+        inMemoryUserRepository.saveUser(user);
+        var admins = inMemoryUserRepository.getAllAdmins();
+        assertEquals(1, admins.size());
+        assertEquals(user, admins.get(0));
+    }
 }
