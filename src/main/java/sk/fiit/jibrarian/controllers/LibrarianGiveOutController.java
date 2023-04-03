df8f872fceaed777a2395ba716f65281dd9f0a6a
Miroslav Todorovic <mtodorovic81@gmail.com>
2023-04-03 17:00:26 +0200

    Added controllers and fxmls for give out and take in functionality

---
+++ src/main/java/sk/fiit/jibrarian/controllers/LibrarianGiveOutController.java
@@ -0,0 +1,16 @@
+package sk.fiit.jibrarian.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LibrarianGiveOutController implements Initializable {
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
