815c7d9d6937768ec090cabd849438de1540964c
oliverhf11 <91140535+oliverhf11@users.noreply.github.com>
2023-04-08 20:13:58 +0200

    Nastavovanie roly pouzivatela v zozname a odstranenie pouzivatela

--- src/main/java/sk/fiit/jibrarian/controllers/AdminScreenUserController.java
+++ src/main/java/sk/fiit/jibrarian/controllers/AdminScreenUserController.java
@@ -1,16 +1,82 @@
 package sk.fiit.jibrarian.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.paint.Paint;
+import sk.fiit.jibrarian.data.RepositoryFactory;
+import sk.fiit.jibrarian.data.UserRepository;
+import sk.fiit.jibrarian.model.Role;
 import sk.fiit.jibrarian.model.User;
 
+import static sk.fiit.jibrarian.model.Role.ADMIN;
+import static sk.fiit.jibrarian.model.Role.LIBRARIAN;
+import static sk.fiit.jibrarian.model.Role.MEMBER;
 
 
 public class AdminScreenUserController {
+    public UserRepository userRepository = RepositoryFactory.getUserRepository();
+
     @FXML
     private Label userId;
+    @FXML
+    private Button removeButton;
+    @FXML
+    private RadioButton rButton1;
+    @FXML
+    private RadioButton rButton2;
+    @FXML
+    private RadioButton rButton3;
 
+    User user;
     public void setData(User user) {
+        this.user=user;
         userId.setText(user.getEmail());
+        if(user.getRole()==ADMIN){
+            rButton1.setSelected(true);
+            rButton2.setSelected(false);
+            rButton3.setSelected(false);
+        }
+        else if (user.getRole()==LIBRARIAN){
+            rButton1.setSelected(false);
+            rButton2.setSelected(true);
+            rButton3.setSelected(false);
+        }
+        else{
+            rButton1.setSelected(false);
+            rButton2.setSelected(false);
+            rButton3.setSelected(true);
+        }
+    }
+
+    public void deleteUser(ActionEvent actionEvent) throws UserRepository.UserNotFound {
+        removeButton.setVisible(false);
+        userId.setTextFill(Paint.valueOf("Red"));
+        userRepository.deleteUser(user);
+    }
+    public void setAdmin(ActionEvent actionEvent) {
+        updateUserRole(ADMIN);
+        System.out.println("admin is set");
+    }
+
+    public void setLibrarian(ActionEvent actionEvent) {
+        updateUserRole(LIBRARIAN);
+        System.out.println("librarian is set");
+    }
+
+    public void SetUser(ActionEvent actionEvent) {
+        updateUserRole(MEMBER);
+        System.out.println("user is set");
+    }
+
+    private void updateUserRole(Role x){
+        user.setRole(x);
+        try {
+            userRepository.updateUser(user);
+        } catch (UserRepository.UserNotFound e) {
+            e.printStackTrace();
+        }
     }
 }
