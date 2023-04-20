2dcaf462f9f67454ebf497c83b8e115edd8ad46a
Nesquiko <lukas.castven@gmail.com>
2023-04-20 12:21:19 +0200

    On delete cascade

--- db/migration/v4__borroweditems.sql
+++ db/migration/v4__borroweditems.sql
@@ -1,8 +1,8 @@
 create table if not exists borrowed_items
 (
     id         uuid not null primary key,
-    user_id    uuid not null references users (id),
-    item_id    uuid not null references items (id),
+    user_id    uuid not null references users (id) on delete cascade,
+    item_id    uuid not null references items (id) on delete cascade,
     until      date not null,
     deleted_at timestamp with time zone
 );
